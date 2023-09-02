c0648a04f9f607a1a031556bda65a343e6b43feb
Jiselle Martins <jisellevms@gmail.com>
2023-09-02 13:16:13 -0300

    consumo da API

--- app/src/main/java/com/jisellemartins/gallery/service/RetrofitService.kt
+++ app/src/main/java/com/jisellemartins/gallery/service/RetrofitService.kt
@@ -1,2 +1,13 @@
 package com.jisellemartins.gallery.service
 
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object service {
+    val api
+        get() = Retrofit.Builder()
+            .baseUrl("https://api.imgur.com/")
+            .addConverterFactory(GsonConverterFactory.create()).build()
+
+    val galleryService = api.create(GalleryService::class.java)
+}
